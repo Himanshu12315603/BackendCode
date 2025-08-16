a2f828c2f14af1273682dd30b64e173145cd5fd2
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2025-08-17 00:11:12 +0530

    add: done with backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
-const express = require('express')
+import express from 'express';
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +9,33 @@ app.get('/login', (req, res) => {
   res.send('You are succesfully Loged in!')
 })
 
+app.get('/jokes', (req, res) => {
+
+  const jokes = [
+      {
+        "id": "1",
+        "firstName": "Tom",
+        "lastName": "Cruise",
+        "photo": "https://jsonformatter.org/img/tom-cruise.jpg"
+      },
+      {
+        "id": "2",
+        "firstName": "Maria",
+        "lastName": "Sharapova",
+        "photo": "https://jsonformatter.org/img/Maria-Sharapova.jpg"
+      },
+      {
+        "id": "3",
+        "firstName": "Robert",
+        "lastName": "Downey Jr.",
+        "photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
+      }
+  ]
+  res.send(jokes);
+})
+
+const port = process.env.PORT || 3000; 
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Serve at http://localhost:${port}`)
 })
